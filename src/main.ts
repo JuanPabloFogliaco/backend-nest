61d3f13c0c21b6257f97182b344e6b24dae5c2ba
JuanPabloFogliaco <juan.pablo.fogliaco@gmail.com>
2023-05-26 16:56:35 -0300

    se centralizo url cors front en env.local

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import * as cors from 'cors';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  await app.use(cors({ origin: 'http://31.220.60.102:3000' }));
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  await app.use(cors({ origin: apiUrl }));
   await app.listen(8080);
 }
 bootstrap();
