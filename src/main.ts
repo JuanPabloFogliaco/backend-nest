6b9a6e4da3e51f9799e54721adf04601707fd5a2
JuanPabloFogliaco <juan.pablo.fogliaco@gmail.com>
2023-05-26 16:20:50 -0300

    Se cambio url cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { HttpExceptionFilter } from './exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.use(cors({ origin: 'http://localhost:3000' }));
+  await app.use(cors({ origin: 'http://31.220.60.102:3000' }));
   await app.listen(8080);
 }
 bootstrap();
